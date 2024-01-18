ffef377c80af955d1775752bf325659b8ec065c3
Michael Mooney <mikeymooney1991@gmail.com>
2024-01-18 16:01:03 -0600

    more cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import {
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Image from "next/image";
-import Link from "next/link";
 import Script from "next/script";
 import { type Metadata } from "next";
 import { TRPCReactProvider } from "@/trpc/react";
@@ -28,23 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <ClerkProvider
-        appearance={{
-          variables: { colorPrimary: "#000000" },
-          elements: {
-            formButtonPrimary:
-              "bg-black border border-black border-solid hover:bg-white hover:text-black",
-            socialButtonsBlockButton:
-              "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
-            socialButtonsBlockButtonText: "font-semibold",
-            formButtonReset:
-              "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
-            membersPageInviteButton:
-              "bg-black border border-black border-solid hover:bg-white hover:text-black",
-            card: "bg-[#fafafa]",
-          },
-        }}
-      >
+      <ClerkProvider>
         <body className={`${inter.className} flex min-h-screen flex-col`}>
           <TRPCReactProvider>
             <header className="flex h-20 items-center gap-4 border-b border-solid border-black border-opacity-20 px-4 sm:px-8">
@@ -69,22 +52,12 @@ export default function RootLayout({
               </SignedIn>
             </header>
             <main className="grow">{children}</main>
-            <footer className="flex h-20 items-center gap-1 border-t px-8 font-medium md:px-20">
-              <Image
-                src="/clerk.svg"
-                alt="Clerk Logo"
-                width={64}
-                height={32}
-                priority
-              />
+            <footer className="flex h-20 items-center justify-end gap-1 border-t p-10 font-medium">
               <span className="text-sm">© 2023</span>
             </footer>
           </TRPCReactProvider>
         </body>
       </ClerkProvider>
-
-      <Script src="https://cdn.jsdelivr.net/npm/prismjs@1/components/prism-core.min.js" />
-      <Script src="https://cdn.jsdelivr.net/npm/prismjs@1/plugins/autoloader/prism-autoloader.min.js" />
     </html>
   );
 }
