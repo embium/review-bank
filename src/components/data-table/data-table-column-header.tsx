2ffecf4de693f2fbd38c223ce859bde5028358b3
Michael Mooney <mikeymooney1991@gmail.com>
2024-01-22 19:28:23 -0600

    remove sorting

--- src/components/data-table/data-table-column-header.tsx
+++ src/components/data-table/data-table-column-header.tsx
@@ -36,49 +36,14 @@ export function DataTableColumnHeader<TData, TValue>({
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button
-            aria-label={
-              column.getIsSorted() === "desc"
-                ? `Sorted descending. Click to sort ascending.`
-                : column.getIsSorted() === "asc"
-                  ? `Sorted ascending. Click to sort descending.`
-                  : `Not sorted. Click to sort ascending.`
-            }
             variant="ghost"
             size="sm"
             className="-ml-3 h-8 data-[state=open]:bg-accent"
           >
             <span>{title}</span>
-            {column.getIsSorted() === "desc" ? (
-              <ArrowDownIcon className="ml-2 h-4 w-4" aria-hidden="true" />
-            ) : column.getIsSorted() === "asc" ? (
-              <ArrowUpIcon className="ml-2 h-4 w-4" aria-hidden="true" />
-            ) : (
-              <CaretSortIcon className="ml-2 h-4 w-4" aria-hidden="true" />
-            )}
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="start">
-          <DropdownMenuItem
-            aria-label="Sort ascending"
-            onClick={() => column.toggleSorting(false)}
-          >
-            <ArrowUpIcon
-              className="mr-2 h-3.5 w-3.5 text-muted-foreground/70"
-              aria-hidden="true"
-            />
-            Asc
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            aria-label="Sort descending"
-            onClick={() => column.toggleSorting(true)}
-          >
-            <ArrowDownIcon
-              className="mr-2 h-3.5 w-3.5 text-muted-foreground/70"
-              aria-hidden="true"
-            />
-            Desc
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
           <DropdownMenuItem
             aria-label="Hide column"
             onClick={() => column.toggleVisibility(false)}
